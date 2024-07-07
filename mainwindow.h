3de063ded0a6a5692b3e2ad1ed2960ebdaf9ccc1
lilinepp <129063961+lilinepp@users.noreply.github.com>
2024-07-07 17:16:03 +0900

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QMessageBox>
+#include <string>
+#include <sstream>
+
+#include "transit.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    //слот обработки входящего числа
+    void processing();
+
+    // вычисление выражения
+    void on_b_equals_clicked();
+
+    // очистка выражения
+    void on_b_clear_clicked();
+
+    // стереть один символ из выражения
+    void on_b_back_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    Transit* main_calc;
+};
+#endif // MAINWINDOW_H
